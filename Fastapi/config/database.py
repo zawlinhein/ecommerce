bf197ed5867f3f330f6cdc313edb069bbb72773f
zawlinhein <105561589+zawlinhein@users.noreply.github.com>
2024-06-24 15:59:52 +0630

    remove mongo connnection string

--- Fastapi/config/database.py
+++ Fastapi/config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client=MongoClient("mongodb+srv://zawlinhein:GTGreen666@cluster0.y1r29u8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client=MongoClient("")
 
 db=client.dummy
 
